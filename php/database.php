a5fe796e70cc709b0a25bca44b21f4b5de18a4d5
Reymar10 <alex.aguado@hotmail.com>
2025-03-04 19:24:50 +0100

    update database usuarios no repetidos

--- php/database.php
+++ php/database.php
@@ -21,13 +21,26 @@ function obtener_todos_los_usuarios($con) {
 function obtener_resultados($resultado) {
     return $resultado->fetch_assoc();
 }
-
 function crear_usuario($con, $usuario, $pass, $nombre, $apellido, $dni, $email, $telefono, $tipo) {
+    // Comprobar si el usuario ya existe
+    $check = $con->prepare("SELECT usuario FROM usuarios WHERE usuario = ?");
+    $check->bind_param("s", $usuario);
+    $check->execute();
+    $resultado = $check->get_result();
+
+    if ($resultado->num_rows > 0) {
+        echo "<script>alert('El usuario ya existe, cambia el nombre de usuario, Por Favor!')</script>";
+        return false; 
+    }
+
+    // Crear usuario si no existe
     $hashed_pass = password_hash($pass, PASSWORD_DEFAULT);
     $stmt = $con->prepare("INSERT INTO usuarios (usuario, pass, nombre, apellido, dni, email, telefono, tipo) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
     $stmt->bind_param("sssssssi", $usuario, $hashed_pass, $nombre, $apellido, $dni, $email, $telefono, $tipo);
     return $stmt->execute();
 }
+
+
 
 function modificar_usuarios($con, $usuario, $nueva_pass, $nuevo_nombre, $nuevo_apellido, $nuevo_dni, $nuevo_email, $nuevo_telefono, $nuevo_tipo) {
     $hashed_pass = password_hash($nueva_pass, PASSWORD_DEFAULT);
