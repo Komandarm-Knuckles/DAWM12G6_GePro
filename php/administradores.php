3e08da01f09d377cda0c2a1c591b73feac11912c
Javi Hernandez Perez <javiperezopos@gmail.com>
2025-03-07 18:07:04 +0000

    editado los espacios entre los componentes de la tabla

--- php/administradores.php
+++ php/administradores.php
@@ -6,62 +6,9 @@ if (!isset($_SESSION['usuario']) || $_SESSION['tipo'] != 0) {
     header("Location: index.php");
     exit();
 }
+
 require_once("database.php");
-
 $con = crearConexion();
-
-/* 
-CREAR USUARIOS
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['usuario'], $_POST['pass'], $_POST['nombre'], $_POST['apellido'], $_POST['dni'], $_POST['email'], $_POST['telefono'], $_POST['tipo'])) {
-    $usuario = $_POST['usuario'];
-    $pass = $_POST['pass'];
-    $nombre = $_POST['nombre'];
-    $apellido = $_POST['apellido'];
-    $dni = $_POST['dni'];
-    $email = $_POST['email'];
-    $telefono = $_POST['telefono'];
-    $tipo = $_POST['tipo'];
-
-    if (empty($usuario) || empty($pass) || empty($nombre) || empty($apellido) || empty($dni) || empty($email) || empty($telefono) || $tipo === '') {
-        echo "<p style='color: red;'>Error: Todos los campos son obligatorios.</p>";
-    } else {
-        if (crear_usuario($con, $usuario, $pass, $nombre, $apellido, $dni, $email, $telefono, $tipo)) {
-            header("Location: administradores.php");
-            exit();
-        } else {
-            echo "<p style='color: red;'>Error al crear usuario.</p>";
-        }
-    }
-}
-*/
-
-/* 
-
-MODIFICAR USUARIOS
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['usuario'], $_POST['nueva_pass'], $_POST['nuevo_nombre'], $_POST['nuevo_apellido'], $_POST['nuevo_dni'], $_POST['nuevo_email'], $_POST['nuevo_telefono'], $_POST['nuevo_tipo'])) {
-
-    $usuario = $_POST['usuario'];
-    $nueva_pass = $_POST['nueva_pass'];
-    $nuevo_nombre = $_POST['nuevo_nombre'];
-    $nuevo_apellido = $_POST['nuevo_apellido'];
-    $nuevo_dni = $_POST['nuevo_dni'];
-    $nuevo_email = $_POST['nuevo_email'];
-    $nuevo_telefono = $_POST['nuevo_telefono'];
-    $nuevo_tipo = $_POST['nuevo_tipo'];
-
-    if (empty($usuario) || empty($nueva_pass) || empty($nuevo_nombre) || empty($nuevo_apellido) || empty($nuevo_dni) || empty($nuevo_email) || empty($nuevo_telefono) || $nuevo_tipo === '') {
-        echo "<p style='color: red;'>Error: Rellena todos los campos.</p>";
-    } else {
-        if (modificar_usuarios($con, $usuario, $nueva_pass, $nuevo_nombre, $nuevo_apellido, $nuevo_dni, $nuevo_email, $nuevo_telefono, $nuevo_tipo)) {
-            header("Location: administradores.php");
-            exit();
-        } else {
-            echo "<p style='color: red;'>Error al modificar usuario.</p>";
-        }
-    }
-}
-*/
 
 // ELIMINAR USUARIOS
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['eliminar_usuario'])) 
@@ -71,7 +18,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['eliminar_usuario']))
         header("Location: " . $_SERVER['PHP_SELF']);
         exit();
     }
-
 ?>
 
 <head>
@@ -82,13 +28,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['eliminar_usuario']))
     <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
 </head>
 <?php
-echo "<body class='h-screen w-full bg-cover bg-center bg-fixed z-10 bg-[url(\"../img/engranajesFondo.jpeg\")]'>";
+echo "<body class='h-screen w-full bg-cover bg-center bg-fixed z-10 bg-[url(\"../img/pixels11.jpg\")]'>";
 // div principal
 echo"<div class='flex w-full h-screen justify-center items-center max-w-screen'>";
 // div Contenedor
 echo"<div class='flex w-full md:flex-row flex-col  justify-center items-center'>";
 // Div Izquierdo
-echo"<div class='flex w-80 md:flex-col md:h-177.5 pl-10 pt-5 bg-orange-500 gap-10'>
+echo"<div class='flex w-80 md:flex-col md:h-169 pl-10 pt-5 bg-orange-500 gap-10'>
         <img class='w-10' src='../img/LogoEmpresa.png' alt='logo Empresa'/>
         <p>GePro</p>
         <p>Reuniones</p>
@@ -99,7 +45,7 @@ echo"<div class='flex w-80 md:flex-col md:h-177.5 pl-10 pt-5 bg-orange-500 gap-1
 // final div izquierdo
 echo"</div>";
 // Div derecho
-echo"<div class='flex flex-col justify-center items-center gap-10 bg-gray-300 p-10'>";
+echo"<div class='flex flex-col  justify-center items-center gap-10 bg-gray-300 p-10'>";
 echo"<h1 class='text-2xl font-bold'>Bienvenido a la Página de Administrador</h1>";
 
 
@@ -116,7 +62,14 @@ $usuarios = obtener_todos_los_usuarios($con);
         echo "<table class='styled-table'>
             <thead>
                 <tr>
-                    <th>USUARIO</th><th>CONTRASEÑA</th><th>NOMBRE</th><th>APELLIDO</th><th>DNI</th><th>EMAIL</th><th>TELEFONO</th><th>TIPO</th><th>ACCIONES</th>
+                    <th class='p-3'>USUARIO</th>
+                    <th class='p-3'>NOMBRE</th>
+                    <th class='p-3'>APELLIDO</th>
+                    <th class='p-3'>DNI</th>
+                    <th class='p-3'>EMAIL</th>
+                    <th class='p-3'>TELEFONO</th>
+                    <th class='p-3'>TIPO</th>
+                    <th class='p-3'>ACCIONES</th>
                 </tr>
             </thead>
             <tbody class='shadow-2xl'>";
@@ -124,18 +77,18 @@ $usuarios = obtener_todos_los_usuarios($con);
         while ($fila = obtener_resultados($usuarios)) {
             extract($fila);
             echo "<tr>
-                    <td class='p-7'>$usuario</td>
-                    <td>$pass</td>
-                    <td>$nombre</td>
-                    <td>$apellido</td>
-                    <td>$dni</td>
-                    <td>$email</td>
-                    <td>$telefono</td>
-                    <td>$tipo</td>";
+                    <td class='text-center p-4'>$usuario</td>
+                    <td class='text-center p-4'>$nombre</td>
+                    <td class='text-center p-4'>$apellido</td>
+                    <td class='text-center p-4'>$dni</td>
+                    <td class='text-center p-4'>$email</td>
+                    <td class='text-center p-4'>$telefono</td>
+                    <td class='text-center p-4'>$tipo</td>";
                     if ($_SESSION['usuario'] !== $usuario) 
                     {
                         echo "
                             <td>
+                                <div class='flex justify-center items-center'>
                                 <form method='POST' action='adminEditarUsuario.php'>
                                     <input type='hidden' name='editar_usuario' value='$usuario'>
                                     <button type='submit' style='background:#007bff;color:white;border:none;cursor:pointer;font-size:14px;float:left;margin-left:2px;' value='Editar'>
@@ -148,13 +101,14 @@ $usuarios = obtener_todos_los_usuarios($con);
                                         <img src='../img/trash-2.png' alt='Eliminar' style='width: 20px; height: 20px;'> 
                                     </button>
                                 </form>
+                                </div>
                             </td>";
                     }
                     else
                     {
                         echo "
                             <td>
-                                <form method='POST' action='adminEditarUsuario.php'>
+                                <form class='flex justify-center items-center' method='POST' action='adminEditarUsuario.php'>
                                     <input type='hidden' name='editar_usuario' value='$usuario'>
                                     <button type='submit' style='background:#007bff;color:white;border:none;cursor:pointer;font-size:14px;float:left;margin-left:2px;' value='Editar'>
                                     <img src='../img/square-pen.png' alt='Eliminar' style='width: 20px; height: 20px;'/> 
@@ -188,62 +142,3 @@ $usuarios = obtener_todos_los_usuarios($con);
 echo"</div></div></div>";
 echo"</body>";
 ?>
-
-
-<!-- 
- CREAR USUARIOS 
-
-<form method='POST' action='' id="admin-createUserForm">
-    <input type='text' name='usuario' placeholder='Nombre de usuario' required><br>
-    <label for="nombre">Nombre:</label>
-    <input type='text' name='nombre' placeholder='Nombre' required><br>
-    <label for="apellido">Apellido:</label>
-    <input type='text' name='apellido' placeholder='Apellido' required><br>
-    <label for="dni">DNI:</label>
-    <input type='text' name='dni' placeholder='DNI' required><br>
-    <label for="email">Email:</label>
-    <input type='email' name='email' placeholder='Email' required><br>
-    <label for="telefono">Teléfono:</label>
-    <input type='number' name='telefono' placeholder='Telefono' required><br>
-    <label for="pass">Contraseña:</label>
-    <input type='password' name='pass' placeholder='Contraseña' required><br>
-    <label for="tipo" id="admin-createUserFormDropdown">Tipo:</label>
-    <select name='tipo' required>
-        <option value=''>Selecciona...</option>
-        <option value='0'>Administrador</option>
-        <option value='1'>Jefe de Equipo</option>
-        <option value='2'>Empleado</option>
-    </select><br>
-    <input type='submit' value='Crear'>
-</form>
--->
-
-<!-- 
-MODIFICAR USUARIOS
-
-<hr>
-<h4>Modificar Usuarios:</h4>
-<form method='POST' action='' id="admin-editUserForm">
-    <label for="usuario">Usuario:</label>
-    <input type='text' name='usuario' placeholder='Usuario a modificar' required><br>
-    <label for="nuevo_nombre">Nombre:</label>
-    <input type='text' name='nuevo_nombre' placeholder='Nuevo Nombre' required><br>
-    <label for="nuevo_apellido">Apellido:</label>
-    <input type='text' name='nuevo_apellido' placeholder='Nuevo Apellido' required><br>
-    <label for="nuevo_dni">DNI:</label>
-    <input type='text' name='nuevo_dni' placeholder='Nuevo DNI' required><br>
-    <label for="nuevo_email">Email:</label>
-    <input type='email' name='nuevo_email' placeholder='Nuevo Email' required><br>
-    <label for="nuevo_telefono">Teléfono:</label>
-    <input type='number' name='nuevo_telefono' placeholder='Nuevo Telefono' required><br>
-    <label for="nueva_pass">Contraseña:</label>
-    <input type='password' name='nueva_pass' placeholder='Nueva Contraseña' required><br>
-    <label>Tipo:</label>
-    <select name='nuevo_tipo' required>
-        <option value='0'>Administrador</option>
-        <option value='1'>Jefe de Equipo</option>
-        <option value='2'>Empleado</option>
-    </select><br>
-    <input type='submit' value='Modificar'>
-</form>
--->
